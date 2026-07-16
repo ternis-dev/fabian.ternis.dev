9b994c95c1f855cd7cc8f4f8dfed1bfd220ea99b
Fabian ternis <f.ternis@xpsystems.eu>
2026-07-16 19:48:48 +0200

    Initial Code-Commit

---
+++ src/index.php
@@ -0,0 +1,6 @@
+Hello, I'm Fabian Ternis – A Student and developer from Germany.
+Building my Home-Lab @ ternis.net
+Webdevelopment @ (xpsystems.eu && ternis-edv.de)
+Links via ternis.link
+
+I just own too many domain (see: dnbx.de)
