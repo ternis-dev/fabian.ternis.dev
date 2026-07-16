17cf722aa0f041f578609c88f73eeb17334dd19a
Fabian ternis <f.ternis@xpsystems.eu>
2026-07-16 20:06:45 +0200

    feat: Extend the randomness and content even further

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 Hello, I'm Fabian Ternis – A Student and developer from Germany.
-Building my Home-Lab @ ternis.net
+Building my HomeLab @ ternis.net
 Webdevelopment @ (xpsystems.eu && ternis-edv.de)
 Links via ternis.link
 
@@ -15,4 +15,34 @@ Whenever i do a disclosure (in any way), i get no response (e.g. 'A Leaked Gemin
 
 <hr>
 
-Building (Half-Vibing) storygrab.net as side-project taht is powered by cloudflare R2(r2.dev) and was solely built to power twins-on-ice.de(alt: mirrortwins.de)
+Building (mostly Vibing) storygrab.net as side-project taht is powered by cloudflare R2(r2.dev) and was solely built to power twins-on-ice.de(alt: mirrortwins.de)
+
+<br>
+<br>
+
+Still sad about loosing the domain 'mail-free.de' in 2025 (currently own mail-free.eu though)
+
+you can contact me via email ('fabian.ternis.dev-{gen_token}[at]fabian.ternismail.de' || '{gen_token}[at]fabian.ternis.dev')
+
+<hr>
+
+Latest Stuff:
+- joined HackClub in July 2026
+
+
+<hr>
+
+What i host on my HomeLab (i know, nobody asked):
+- WireGuard (Both internally and Externally)
+- Pi-Hole (just about the average hole)
+- Immich (no docker-images but pictures instead)
+- NextCloud (The coud of teh future)
+- GiTea (Great Drink)
+- Docker (Just some Containers arriving)
+[- RoundCube (Didn't know taht was possible)]
+- n8n (Whatever taht is)
+
+
+<hr>
+
+Device Specs (screenfetch)
