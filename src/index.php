24b40574f6bb44fcec69a55c2e16e722039c6246
Fabian ternis <f.ternis@xpsystems.eu>
2026-07-19 15:27:54 +0200

    feat: hero on desktop close to finsihed

--- src/index.php
+++ src/index.php
@@ -1,56 +1,58 @@
-<section id="hero">
-    <h1>Hello, I'm Fabian Ternis</h1>
-    <h2>A Student and developer from Germany.</h2>
-    <div class="some-container">
-        <h3>Building my HomeLab @ <a href="http://ternis.net">ternis.net</a></h3>
-        <h3>Webdevelopment @ <span class="font-code">(<a href="http://xpsystems.eu" target="_blank">xpsystems.eu</a> && <a href="http://ternis-edv.de">ternis-edv.de</a>)</span></h3>
-        <h3>Links via <a href="http://fabian-ternis-dev.ternis.link" target="_blank">ternis.link</a></h3>
-        <h3>I just own too many domain (see: <a href="http://dnbx.de#domainlist" target="_blank">dnbx.de</a>)</h3>
-        <div class="img-container">
-            <img src="BASF_2026.jpg" class="me" alt="Fabian Ternis at BASF SE in Ludwigshafen during the Jugend Forscht State Competition / Landeswettbewerb Rheinland-Pfalz">            <span class="copyright-note">Image by <a href="https://basf.com" target="_blank">BASF SE</a></span>
+<main>
+    <section id="hero">
+        <h1>Hello, I'm Fabian Ternis</h1>
+        <h2>A Student and developer from Germany.</h2>
+        <div class="some-container">
+            <h3>Building my HomeLab @ <a href="http://ternis.net">ternis.net</a></h3>
+            <h3>Webdevelopment @ <span class="font-code">(<a href="http://xpsystems.eu" target="_blank">xpsystems.eu</a> && <a href="http://ternis-edv.de">ternis-edv.de</a>)</span></h3>
+            <h3>Links via <a href="http://fabian-ternis-dev.ternis.link" target="_blank">ternis.link</a></h3>
+            <h3>I just own too many domain (see: <a href="http://dnbx.de#domainlist" target="_blank">dnbx.de</a>)</h3>
+            <div class="img-container">
+                <img src="BASF_2026.jpg" class="me" alt="Fabian Ternis at BASF SE in Ludwigshafen during the Jugend Forscht State Competition / Landeswettbewerb Rheinland-Pfalz">            <span class="copyright-note">Image by <a href="https://basf.com" target="_blank">BASF SE</a></span>
+            </div>
         </div>
-    </div>
-</section>
-
-<section>
-    I am also trying not to get sued by cult-management.com for owning ('twins-on-ice.de' && 'mirrortwins.de' && 'twinsonice.eu') as well as ('emiliamacula.de' && 'letiziamacula.de')
+    </section>
+    
+    <section>
+        I am also trying not to get sued by cult-management.com for owning ('twins-on-ice.de' && 'mirrortwins.de' && 'twinsonice.eu') as well as ('emiliamacula.de' && 'letiziamacula.de')
+        <hr>
+        Whenever i do a disclosure (in any way), i get no response (e.g. 'A Leaked Gemini-API-Key by a Google-Employee' or 'no idea how to name it')
+        <hr>
+        Building (mostly Vibing) storygrab.net as side-project taht is powered by cloudflare R2(r2.dev) and was solely built to power twins-on-ice.de(alt: mirrortwins.de)
+    </section>
+    
+    <section>
+    Still sad about loosing the domain 'mail-free.de' in 2025 (currently own mail-free.eu though)
+    
+    you can contact me via email ('fabian.ternis.dev-{gen_token}[at]fabian.ternismail.de' || '{gen_token}[at]fabian.ternis.dev')
+    </section>
+    <section id="news">
+    Latest Stuff:
+    - joined HackClub in July 2026
+    </section>
+    
+    <section id="homelab">
+    What i host on my HomeLab (i know, nobody asked):
+    - WireGuard (Both internally and Externally)
+    - Pi-Hole (just about the average hole)
+    - Immich (no docker-images but pictures instead)
+    - NextCloud (The coud of teh future)
+    - GiTea (Great Drink)
+    - Docker (Just some Containers arriving)
+    [- RoundCube (Didn't know taht was possible)]
+    - n8n (Whatever taht is)
+    
+    </section>
+    
     <hr>
-    Whenever i do a disclosure (in any way), i get no response (e.g. 'A Leaked Gemini-API-Key by a Google-Employee' or 'no idea how to name it')
+    
+    Device Specs (screenfetch)
+    
+    
     <hr>
-    Building (mostly Vibing) storygrab.net as side-project taht is powered by cloudflare R2(r2.dev) and was solely built to power twins-on-ice.de(alt: mirrortwins.de)
-</section>
-
-<section>
-Still sad about loosing the domain 'mail-free.de' in 2025 (currently own mail-free.eu though)
-
-you can contact me via email ('fabian.ternis.dev-{gen_token}[at]fabian.ternismail.de' || '{gen_token}[at]fabian.ternis.dev')
-</section>
-<section id="news">
-Latest Stuff:
-- joined HackClub in July 2026
-</section>
-
-<section id="homelab">
-What i host on my HomeLab (i know, nobody asked):
-- WireGuard (Both internally and Externally)
-- Pi-Hole (just about the average hole)
-- Immich (no docker-images but pictures instead)
-- NextCloud (The coud of teh future)
-- GiTea (Great Drink)
-- Docker (Just some Containers arriving)
-[- RoundCube (Didn't know taht was possible)]
-- n8n (Whatever taht is)
-
-</section>
-
-<hr>
-
-Device Specs (screenfetch)
-
-
-<hr>
-
-Some other projects i am currently not working on:
-- Web-Search.org (Just anotehr open-source Searchengine)
-- MTEX.dev (Developer Tools (SchemaBuilder(which i am currently working on) might be one of thiose but is currently not beeing built under the MTEX.dev Brand))
-... way too many more ...
+    
+    Some other projects i am currently not working on:
+    - Web-Search.org (Just anotehr open-source Searchengine)
+    - MTEX.dev (Developer Tools (SchemaBuilder(which i am currently working on) might be one of thiose but is currently not beeing built under the MTEX.dev Brand))
+    ... way too many more ...
+</main>
